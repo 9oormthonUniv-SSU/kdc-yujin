190f017d2deef97c82ae250b324411dafd3d52c4
nijuy <youjin6325@naver.com>
2024-09-05 14:26:12 +0900

    2주차: GuessNumbers 클래스 테스트 코드 작성

--- site_1/lotto/tests.py
+++ site_1/lotto/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from lotto.models import GuessNumbers
 
-# Create your tests here.
+class GuessNumbersTestCase(TestCase):
+   def test_generate(self):
+    g = GuessNumbers(name='Test numbers', text='selected numbers')
+    g.generate()
+    
+    print(g.update_date)
+    print(g.lottos)
+
+    # default로 6개 숫자 x 5 set = 30개의 숫자가 생성됨을 확인
+    self.assertTrue(len(g.lottos) > 20) # (OK or FAILED)
