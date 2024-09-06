dc0f36b6d78e35677304133b5f64745bd80d58ab
nijuy <youjin6325@naver.com>
2024-09-06 12:27:33 +0900

    3주차: views.py, Template, Model 연동하기

--- site_1/lotto/views.py
+++ site_1/lotto/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lotto.models import GuessNumbers
 
 def index(request):
- return render(request, 'lotto/default.html', {})
+ lottos = GuessNumbers.objects.all()
+ return render(request, 'lotto/default.html', {'lottos': lottos})
 
 def hello(request):
  return HttpResponse("<h1 style='color:red;'>Hello, world! 🥸</h1>")
