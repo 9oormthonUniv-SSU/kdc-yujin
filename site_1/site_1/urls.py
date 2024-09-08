3a605ab33bbf49b6bd241f64a653d0e5a3ac6e38
nijuy <youjin6325@naver.com>
2024-09-08 20:00:41 +0900

    3주차: detail.html 생성

--- site_1/site_1/urls.py
+++ site_1/site_1/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello, name='hello_main'),
     path('lotto/', views.index, name='index'),
-    path('lotto/new', views.post, name="new_lotto")
+    path('lotto/new', views.post, name="new_lotto"),
+    path('lotto/<int:lottokey>/detail', views.detail, name='detail')
 ]
