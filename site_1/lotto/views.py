ae5ce1bab4437acb0c7afcb6375224940e16ce75
nijuy <youjin6325@naver.com>
2024-09-08 19:53:44 +0900

    3주차: POST 요청 처리하기

--- site_1/lotto/views.py
+++ site_1/lotto/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from lotto.models import GuessNumbers
 from .forms import PostForm
 
 def index(request):
- lottos = GuessNumbers.objects.all()
- return render(request, 'lotto/default.html', {'lottos': lottos})
+   lottos = GuessNumbers.objects.all()
+   return render(request, 'lotto/default.html', {'lottos': lottos})
 
 def hello(request):
- return HttpResponse("<h1 style='color:red;'>Hello, world! 🥸</h1>")
+    return HttpResponse("<h1 style='color:red;'>Hello, world! 🥸</h1>")
 
 def post(request):
- form = PostForm()
- return render(request, "lotto/form.html", {"form": form})
+    if request.method == "POST":
+        print(request.POST)
+        print(request.method)
+
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            lotto = form.save(commit = False) 
+            print(type(lotto)) # <class 'lotto.models.GuessNumbers'>
+            print(lotto)
+            lotto.generate()
+            return redirect('index')
+        
+    else:
+        form = PostForm()
+        return render(request, "lotto/form.html", {"form": form})
