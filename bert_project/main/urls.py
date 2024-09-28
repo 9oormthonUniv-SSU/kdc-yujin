734dfc62e16d78f7c193c802cc946419b92616bd
nijuy <youjin6325@naver.com>
2024-09-28 20:37:22 +0900

    8주차: 텍스트 감정 분류 적용하기

--- bert_project/main/urls.py
+++ bert_project/main/urls.py
@@ -5,4 +5,5 @@ app_name = 'main'
 
 urlpatterns = [
     path('bert_input/', views.bert_input, name='bert_input'),
+    path('bert_predict/', views.bert_predict, name='bert_predict'),
 ]
--- bert_project/main/views.py
+++ bert_project/main/views.py
@@ -3,12 +3,14 @@ from django.utils import timezone # settings.py의 timezone을 따라감
 from django.conf import settings
 
 import pandas as pd
+from . import inference_bert
+from . import bert_load
 
 def index(request):
     current_time = timezone.now()
     context = {'current_time':current_time}
-    
     return render(request, 'main/index.html', context)
+
 
 def bert_input(request):
     reviews_df = pd.read_table('./data/naver_shopping.txt', names=['ratings', 'reviews'])
@@ -24,3 +26,16 @@ def bert_input(request):
 
     context = {'reviews_df_html':reviews_df_html}
     return render(request, 'main/bert_input.html', context)
+
+
+def bert_predict(request):
+    target_sentence = request.POST['target_sentence']
+
+    tokenizer = settings.TOKENIZER_KOBERT
+    model = settings.MODEL_KOBERT
+
+    result_bert = inference_bert.predict_sentiment(target_sentence, tokenizer, model)
+
+    context = {'target_sentence':target_sentence,
+               'result_bert':result_bert}
+    return render(request, 'main/bert_predict.html', context)
